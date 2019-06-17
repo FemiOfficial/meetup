532ff8700c30ee4519c4c337ae5433c1ee2f1977
unknown <alayesanmifemi@gmail.com>
2019-06-18 00:43:17 +0100

    added db credentials

--- server/database/db.js
+++ server/database/db.js
@@ -1,8 +1,22 @@
 const Sequelize = require("sequelize");
 const db = {};
-const sequelize = new Sequelize("meet_upnode",
-        "root", "", {
-            host: "localhost",
+// const sequelize = new Sequelize("meet_upnode",
+//         "root", "", {
+//             host: "localhost",
+//             dialect: "mysql",
+//             operatorsAliases: false,
+
+//             pool: {
+//                 max: 5,
+//                 min: 0,
+//                 acquire: 30000,
+//                 idle: 10000
+//             }
+//         })
+
+const sequelize = new Sequelize("heroku_c27a069b2f6b005",
+        "b97c9dff61b4b0", "0bd03b8f", {
+            host: "us-cdbr-iron-east-02.cleardb.net",
             dialect: "mysql",
             operatorsAliases: false,
 
